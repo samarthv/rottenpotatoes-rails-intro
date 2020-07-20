3bfb117df93727285efd4bb175d3396b2d8fd220
Samarth Venkatasubramaniam <samarthv@berkeley.edu>
2020-07-20 23:48:48 +0000

    getting rid of work in movies contorller bc it  broke it

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,27 +11,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-    sort = params[:sort] || session[:sort]
-    if sort == 'title'
-      ordering,@title_header = {:title => :asc}, 'hilite'
-    end
-    if sort == 'release_date'
-      ordering,@date_header = {:release_date => :asc}, 'hilite'
-    end
-    
-    @all_ratings = Movie.all_ratings
-    @selected_ratings = params[:ratings] || session[:ratings] || {}
-
-    if @selected_ratings == {}
-      @selected_ratings = Hash[@all_ratings.map {|rating| [rating, rating]}]
-    end
-
-    if params[:sort] != session[:sort] or params[:ratings] != session[:ratings]
-      session[:sort] = sort
-      session[:ratings] = @selected_ratings
-      redirect_to :sort => sort, :ratings => @selected_ratings and return
-    end
-    @movies = Movie.where(rating: @selected_ratings.keys).order(ordering)
+    @movies = Movie.all
   end
 
   def new
