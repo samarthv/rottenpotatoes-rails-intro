98c7d704c25e838b74fcef45e23e5de5ab699f9e
Samarth Venkatasubramaniam <samarthv@berkeley.edu>
2020-07-21 20:36:43 +0000

    trying to implement movies controller to sort

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,20 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    sort = params[:sort] || session[:sort]
+
+    @all_ratings = Movie.all_ratings
+    @selected_ratings = params[:ratings] || session[:ratings] || {}
+
+    if @selected_ratings == {}
+      @selected_ratings = Hash[@all_ratings.map {|rating| [rating, 1]}]
+    end
+
+    if params[:sort] != session[:sort] or params[:ratings] != session[:ratings]
+      session[:sort] = sort
+      session[:ratings] = @selected_ratings
+    end
+    @movies = Movie.where(rating: @selected_ratings.keys).order(sort)
   end
 
   def new
